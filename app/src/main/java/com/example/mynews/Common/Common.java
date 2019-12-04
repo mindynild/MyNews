0fb6c61ea27be3ba21c9b05c2bc2eb662d12ab2e
Indya Pereira <indya.pereira@gmail.com>
2019-12-04 17:20:35 +0100

    create source_layout

--- app/src/main/java/com/example/mynews/Common/Common.java
+++ app/src/main/java/com/example/mynews/Common/Common.java
@@ -7,6 +7,9 @@ import retrofit2.Retrofit;
 
 public class Common {
     private static final String BASE_URL="https://newsapi.org/";
+
+    public static final String API_KEY="6bbed62e53d042cb91b21b663a178b2f";
+
     public static NewsService getNewsService() {
         return RetrofitClient.getClient(BASE_URL).create(NewsService.class);
     }
